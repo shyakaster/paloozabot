225f242b2c9d9d6686d6ef1829bfac1bb563fa6a
aradmschool <arad.n@masterschool.com>
2024-04-17 19:58:47 -0400

    first commit for bot.py

---
+++ bot.py
@@ -0,0 +1,23 @@
+import os
+import discord
+from discord.ext import commands
+
+from dotenv import load_dotenv
+_ = load_dotenv()
+auth_token = os.environ['discord_token']
+
+intents = discord.Intents.default()
+intents.messages = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send('Hello there!')
+
+bot.run(auth_token)
